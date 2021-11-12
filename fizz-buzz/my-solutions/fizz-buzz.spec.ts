6cdca1d2717023ca0d5b59ccfae3ace0d40a23f9
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2021-11-12 18:20:11 +0100

    [fizz-buzz] Added case with fizz result: start index is 3

--- fizz-buzz/my-solutions/fizz-buzz.spec.ts
+++ fizz-buzz/my-solutions/fizz-buzz.spec.ts
@@ -8,4 +8,12 @@ describe('fizz-buzz.spec', () => {
 
     expect(result).toEqual([1])
   })
+
+  it('should return a list with items 1,2,fizz when start index is 3', () => {
+    const given = 3
+
+    const result = fizzBuzz(given)
+
+    expect(result).toEqual([1, 2, 'fizz'])
+  })
 })
--- fizz-buzz/my-solutions/fizz-buzz.ts
+++ fizz-buzz/my-solutions/fizz-buzz.ts
@@ -1,3 +1,15 @@
 export function fizzBuzz(startAt: number): Array<number | string> {
-  return [startAt]
+  const result: Array<number | string> = []
+
+  for (let i = 1; i <= startAt; i++) {
+    let item: number | string = i
+
+    if (i % 3 === 0) {
+      item = 'fizz'
+    }
+
+    result.push(item)
+  }
+
+  return result
 }
